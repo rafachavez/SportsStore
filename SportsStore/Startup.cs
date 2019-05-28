f15877116d68ca2ab12f947d76d60a1ff2639705
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-05-27 21:34:21 -0600

    Enabled features on the startup file

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,12 +15,20 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+
+            });
+
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -28,7 +36,7 @@ namespace SportsStore
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
-            });
+            });*/
         }
     }
 }
