d0855aea15b371ac722a044ad474559595a6cb70
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-04 01:20:42 -0600

    added currentcategory attribute to productlistviewmodel

--- SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using SportsStore.Models;
 
 namespace SportsStore.Models.ViewModels
 {
+
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
