da68bc0773c9f69776bf70c4a0d553c3bd32113c
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-04 22:27:36 -0600

    updated startup to enable sessions

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -28,6 +28,8 @@ namespace SportsStore
                     Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -35,6 +37,7 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes => {
 
                 routes.MapRoute(
