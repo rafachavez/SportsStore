4cba5a9216754db015d2beec34d7a834499d84fa
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-04 21:31:42 -0600

    created components folder and added navigationmenu

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components
+{
+
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
