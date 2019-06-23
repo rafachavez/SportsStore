d4056a47fdae0157b40d8fb8363e34da6e99a89b
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-22 19:59:16 -0600

    updated adminController by adding the edit action

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
@@ -14,5 +15,9 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult Index() => View(repository.Products);
+
+        public ViewResult Edit(int productId) =>
+           View(repository.Products
+               .FirstOrDefault(p => p.ProductID == productId));
     }
 }
